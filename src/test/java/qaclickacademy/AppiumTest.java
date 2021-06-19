ce7ebc59b203e6afac261cf5a68ca53cb080e582
anupama <anupamadinoop@gmail.com>
2021-06-18 23:10:15 -0400

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,14 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+    @Test
+    public void nativeAPAndroid(){
+        System.out.println("NativeAPAndroid");
+    }
+    @Test
+    public void IOSApps(){
+        System.out.println("IOSApps");
+    }
+}
